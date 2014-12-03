a4c6c045577bef535667c90f55e3d294e04972b6
dmassiani <davidmassianirennes@gmail.com>
2014-12-03 16:45:45 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Macro Content Hammer
+ */
+/*
+Plugin Name: Macro Content Hammer
+Plugin URI: http://macrocontenthammer.com/
+Description: Used by millions, Akismet is quite possibly the best way in the world to <strong>protect your blog from comment and trackback spam</strong>. It keeps your site protected from spam even while you sleep. To get started: 1) Click the "Activate" link to the left of this description, 2) <a href="http://akismet.com/get/">Sign up for an Akismet API key</a>, and 3) Go to your Akismet configuration page, and save your API key.
+Version: 1.0
+Author: David Massiani
+Author URI: http://davidmassiani.com
+License: GPLv2 or later
+Text Domain: macrocontenthammer
+*/
