eeaec4ede8c61b3212b109cd8bcad56c47e0774d
dmassiani <davidmassianirennes@gmail.com>
2015-01-02 16:14:29 +0100

    update example

--- front/macro-template-example.php
+++ front/macro-template-example.php
@@ -5,6 +5,7 @@ Template Name: powerfullmacro
 Description: 2 columns with 2 content and 2 images
 
 -----------------------------------------------------------------------------------
+{"type": "title", "name": "Titre", "slug": "title"}
 {"type": "editor", "name": "left content", "slug": "left_content"}
 {"type": "image", "name": "left illustration", "slug": "left_illustration"}
 {"type": "editor", "name": "right content", "slug": "right_content"}
@@ -14,12 +15,12 @@ Description: 2 columns with 2 content and 2 images
 */
 ?>
 
-<h2>Powerfull Macro Content Exemple</h2>
+<h2><?php the_chapter_title('title') ?></h2>
 
 <h3>Left content</h3>
 <?php the_chapter('left_content') ?>
 
-<h3>Left image</h3>
+<h3>Left illustration</h3>
 <?php the_illustration('left_illustration') ?>
 
 <h3>Right content</h3>
